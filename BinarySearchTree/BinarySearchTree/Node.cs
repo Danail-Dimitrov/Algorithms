e2220000a8a44798d29c410a4c459b5a8a6375a1
Danail Dimitrov <danaildimitrov321@gmail.com>
2020-09-27 00:29:34 +0300

    adding part of BST

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Node<T> where T : IComparable
+    {
+        public Node()
+        {
+
+        }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public Node<T> Left { get; set; }
+
+        public Node<T> Right { get; set; }
+
+        public Node<T> Perant { get; set; }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.Insert(17);
+            binarySearchTree.Insert(9);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(25);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(31);
+            binarySearchTree.Insert(20);
+
+            binarySearchTree.Remove(3);
+
+            binarySearchTree.InOrder();
+        }
+    }
+}
