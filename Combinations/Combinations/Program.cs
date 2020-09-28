ec0170247898c2af36b24bd14a2914299284465f
Danail Dimitrov <danaildimitrov321@gmail.com>
2020-09-28 11:55:07 +0300

    Adding variations with repetitions and combinations

---
+++ Combinations/Combinations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        private static int slots = 2;
+        private static string[] arr = { "A", "B", "C" };
+        private static string[] reslut = new string[slots];
+
+        static void Main(string[] args)
+        {
+            Comb(0, 0);
+        }
+        
+        static void Comb(int index, int setIndex)
+        {
+            if(index >= slots)
+            {
+                Console.WriteLine(string.Join(' ', reslut));
+            }
+            else
+            {
+                for(int i = setIndex; i < arr.Length; i++)
+                {
+                    reslut[index] = arr[i];
+                    Comb(index + 1, i + 1);
+                }      
+            }           
+        }
+    }
+}
---
+++ VariationsWithRepetition/VariationsWithRepetition/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace VariationsWithRepetition
+{
+    class Program
+    {
+        private static int slots = 2;
+        private static string[] arr = { "A", "B", "C" };
+        private static string[] reslut = new string[slots];
+
+        static void Main(string[] args)
+        {
+            Var(0);
+        }
+
+        static void Var(int index)
+        {
+            if(index >= slots)
+            {
+                Console.WriteLine(string.Join(' ', reslut));
+            }
+            else
+            {
+                for(int i = 0; i < arr.Length; i++)
+                {
+                    reslut[index] = arr[i];
+                    Var(index + 1);
+                }
+            }
+        }
+    }
+}
